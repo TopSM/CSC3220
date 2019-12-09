7145116843faf7360b1a3daf0a062ab8add70fe9
TopSM <ostavo.p@gmail.com>
2019-12-09 18:05:06 -0500

    final

---
+++ Project/my-app/backend/Models/orderSchema.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6 */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+    seller_id: String,
+    user_id: String,
+    foodItems: [String],
+    orderPlaced: {
+        type: Date,
+        default: Date.now()
+    },
+    delivery_location: {
+        address: {
+            street: String,
+            city: String,
+            state: String
+        },
+        lat: Number,
+        long: Number
+    },
+    orderStatus: {
+        orderReceived: Boolean,
+        orderPrepping: Boolean,
+        outForDelivery: Boolean,
+        delivered: Boolean
+    }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
