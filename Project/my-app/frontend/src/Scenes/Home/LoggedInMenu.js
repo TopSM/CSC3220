a1de9e5df7eabcfa3f55715d5c4f6b721d355069
TopSM <ostavo.p@gmail.com>
2019-12-10 14:40:03 -0500

    comments

--- Project/my-app/frontend/src/Scenes/Home/LoggedInMenu.js
+++ Project/my-app/frontend/src/Scenes/Home/LoggedInMenu.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { changeLogged, changeAccountType } from '../../actions/account-actions';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { changeLogged, changeAccountType } from "../../actions/account-actions";
+import IconButton from "material-ui/IconButton";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
 
 class LoggedInMenu extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleSignOut = this.handleSignOut.bind(this);
     this.logOutUser = this.logOutUser.bind(this);
     this.removeAccountType = this.removeAccountType.bind(this);
   }
-  logOutUser = (data) => {
+  logOutUser = data => {
     this.props.logOutUser(data);
-  }
+  };
   removeAccountType = () => {
-    this.props.removeAccountType('');
-  }
+    this.props.removeAccountType("");
+  };
   handleSignOut = () => {
     this.removeAccountType();
     this.logOutUser(false);
-    localStorage.removeItem('api_token');
-  }
+    localStorage.removeItem("api_token");
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <IconMenu
         iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        targetOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "top" }}
       >
-        <MenuItem onClick={this.handleClose} href="/MyAccount">My Account</MenuItem>
-        {this.props.accountType == 'seller' && <MenuItem onClick={this.handleClose} href="/MySellerPortal">My Store</MenuItem>}
+        <MenuItem onClick={this.handleClose} href="/MyAccount">
+          My Account
+        </MenuItem>
+        {this.props.accountType === "seller" && (
+          <MenuItem onClick={this.handleClose} href="/MySellerPortal">
+            My Store
+          </MenuItem>
+        )}
         <MenuItem onClick={this.handleSignOut} primaryText="Sign out" />
       </IconMenu>
-    )
+    );
   }
-};
+}
 
 const mapStateToProps = state => {
   return {
